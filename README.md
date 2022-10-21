209e5fc0ec7c4afb856ad9e2c5214f62cdcc24ac
Peterforyou <110077295+Peterforyou@users.noreply.github.com>
2022-10-21 19:44:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# A Song App
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+This is a song app where you get to play your favorite artist preview song.
+
+### Screenshot
+
+![](./Annotation%202022-10-21%20193829.png)
+
+### Links
+
+- Live Site URL:(https://peterforyou.github.io/House-Company/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+### Continued development    
+
+I would love to focus more on React
+
+## Author
+
+- Name - [Peter Bit](https://www.twitter.com/Peterbyte2)
+
+## Acknowledgments
+
+Am aknowledging my humble self for picking up this challenge.
---
+++ app.js
@@ -0,0 +1,70 @@
+// let me = fetch('https://itunes.apple.com/search?')
+
+// setTimeout(() => {
+//     console.log(me);
+// }, 2000);
+
+let songContainer = document.querySelector('.container')
+let searchMusic;
+let updatedSearchMusic = (e) => {
+    searchMusic = document.querySelector('input').value
+     if (!searchMusic || searchMusic === '') {
+        alert('Please search for a song')
+     } else {
+        while (songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
+        }
+                
+        fetch(`https://itunes.apple.com/search?media=music&term=${searchMusic}`)
+        .then((response) =>
+            response.json()
+        )
+        .then((data) => {
+            const musicData = data.results;
+            return musicData.map((result) => {
+
+
+                const div = document.createElement('div'),
+                    image = document.createElement('img'),  
+                    artist = document.createElement("p"),  
+                    song = document.createElement("p"),  
+                    audio = document.createElement("audio"),  
+                    audiosrc = document.createElement("source")
+                    console.log(result);
+
+                    image.src = result.artworkUrl100;
+                    artist.innerHTML = result.artistName;
+                    song.innerHTML = result.trackName;
+                    audiosrc.src = result.previewUrl;
+                    audio.setAttribute('controls', '');
+
+                    div.appendChild(image);
+                    div.appendChild(artist);
+                    div.appendChild(song);
+                    audio.appendChild(audiosrc);
+                    div.appendChild(audio);
+
+                    songContainer.appendChild(div);
+
+            })
+        })
+        .catch((error) => {
+            console.log('this ia an error', error)
+        })
+
+     }
+     e.preventDefault()
+}
+
+let btn = document.querySelector('button')
+btn.addEventListener('click', updatedSearchMusic)
+
+document.addEventListener('play', (e) => {
+    let stopMusic = document.getElementsByTagName('audio')
+
+    for (let i = 0; i < stopMusic.length; i++){
+        if (stopMusic[i] != e.target) {
+            stopMusic[i].pause();
+        }
+    }
+}, true)
